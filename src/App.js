bc7a4b3ce94666329a05f8abea08a443baf949b2
milimk <milimkim@gmail.com>
2024-09-12 10:53:33 -0700

    Added footer links

--- src/App.js
+++ src/App.js
@@ -6,6 +6,21 @@ export default function App() {
       <header>
         <h1>React Weather App</h1>
       </header>
+      <main></main>
+      <footer>
+        This project was coded by{" "}
+        <a href="https://github.com/milimk" target="_blank" rel="noreferrer">
+          MiliM
+        </a>{" "}
+        and it's{" "}
+        <a
+          href="https://github.com/milimk/react-final-weather-app"
+          target="_blank"
+          rel="noreferrer"
+        >
+          open-sourced
+        </a>{" "}
+      </footer>
     </div>
   );
 }
