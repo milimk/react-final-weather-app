c3c39b23ba605d2e4df80d39c77fb0e8d18bdb52
milimk <milimkim@gmail.com>
2024-09-12 11:13:01 -0700

    Added Bootstrap

--- src/App.js
+++ src/App.js
@@ -3,24 +3,26 @@ import "./App.css";
 export default function App() {
   return (
     <div className="App">
-      <header>
-        <h1>React Weather App</h1>
-      </header>
-      <main></main>
-      <footer>
-        This project was coded by{" "}
-        <a href="https://github.com/milimk" target="_blank" rel="noreferrer">
-          MiliM
-        </a>{" "}
-        and it's{" "}
-        <a
-          href="https://github.com/milimk/react-final-weather-app"
-          target="_blank"
-          rel="noreferrer"
-        >
-          open-sourced
-        </a>{" "}
-      </footer>
+      <div className="container">
+        <header>
+          <h1>React Weather App</h1>
+        </header>
+        <main></main>
+        <footer>
+          This project was coded by{" "}
+          <a href="https://github.com/milimk" target="_blank" rel="noreferrer">
+            MiliM
+          </a>{" "}
+          and it's{" "}
+          <a
+            href="https://github.com/milimk/react-final-weather-app"
+            target="_blank"
+            rel="noreferrer"
+          >
+            open-sourced
+          </a>{" "}
+        </footer>
+      </div>
     </div>
   );
 }
